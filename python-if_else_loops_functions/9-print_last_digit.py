4727ec1866709ddbd18c5a9787ed2d3d410aca8b
MaxB-Holberton <12107@holbertonstudents.com>
2026-01-23 10:58:07 +1100

    correction updates x2

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
     if number > 0:
-        print(number % 10, end="")
+        print("{:d}".format(number % 10), end="")
         return number % 10
     else:
-        print((number * -1) % 10, end="")
+        print("{:d}".format((number * -1) % 10), end="")
         return (number * -1) % 10
