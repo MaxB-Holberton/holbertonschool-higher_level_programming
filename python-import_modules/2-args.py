85befa4cf143dded483d0ec79df16703ae11ac4b
MaxB-Holberton <12107@holbertonstudents.com>
2026-01-24 15:03:51 +1100

    minor changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
     args_len = len(args_list)
     if (args_len == 1):
         print("0 argument.")
+    elif:
+        print("{} argument:".format(args_len - 1))
+         for i in range(1, args_len):
+            print("{}: {}".format(i, args_list[i]))
     else:
         print("{} arguments:".format(args_len - 1))
         for i in range(1, args_len):
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     args_len = len(args_list)
     total = 0
     if (args_len == 1):
-        print("{}".format(sum))
+        print("{}".format(total))
     else:
         for i in range(1, args_len):
             total += int(args_list[i])
