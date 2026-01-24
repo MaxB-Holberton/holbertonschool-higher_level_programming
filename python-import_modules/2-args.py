d50022902fff3643fd7ba9f6d271fa71ebaac009
MaxB-Holberton <12107@holbertonstudents.com>
2026-01-24 15:14:54 +1100

    minor changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     args_list = sys.argv
     args_len = len(args_list)
     if (args_len == 1):
-        print("0 argument.")
-    elif(args_len == 2):
+        print("0 arguments.")
+    elif (args_len == 2):
         print("{} argument:".format(args_len - 1))
         for i in range(1, args_len):
             print("{}: {}".format(i, args_list[i]))
