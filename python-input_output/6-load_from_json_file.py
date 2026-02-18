c7470b7d1df2cf2dc499b5ffee16467273a83692
MaxB-Holberton <12107@holbertonstudents.com>
2026-02-18 13:58:43 +1100

    updates to task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-    Module to convert JSON strings to python objects
+    Module to read json string from a file
 """
 
 from json import loads
 
 
 def load_from_json_file(filename):
+    """
+        read from a json file
+    """
     with open(filename, mode="r", encoding='utf-8') as f:
         return loads(f.read())
