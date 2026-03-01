4c45946c90670aecbce8e70e9786f0989d0e1fa9
MaxB-Holberton <12107@holbertonstudents.com>
2026-03-01 20:31:50 +1100

    updates to tasks x1

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- show all the genres that are tagged for Dexter
 SELECT tv_genres.name FROM tv_genres
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+INNER JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC
--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- show all the tv shows and their genres
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
-JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
-JOIN tv_gernmes ON tv_show_genres.genres_id = tv_genres.id
+LEFT JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_gernmes ON tv_show_genres.genres_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
