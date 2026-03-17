8361c97174aef36f10033cf3b3c6fa866aea843e
MaxB-Holberton <12107@holbertonstudents.com>
2026-03-17 15:34:44 +1100

    Minor fixes for task 11 x2

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,8 @@ const argvLen = argv.length;
 if (argvLen === 2 || argvLen === 3) {
   console.log(0);
 } else {
-  const item = argv.slice(2);
-  item.map(Number).sort();
+  const item = argv.slice(2).map(Number);
+  item.sort((a, b) => a - b);
+  console.log(item);
   console.log(item[item.length - 2]);
 }
